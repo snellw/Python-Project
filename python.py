a845a6f3a425a15f601a48bc13d861dc5485568b
Matt Brigida <mdbrigida@gmail.com>
2020-03-06 20:52:29 -0500

    some needed packages

--- python.py
+++ python.py
@@ -1,1 +1,4 @@
-Hello world
+### Hello world
+### We'll need to install a few python libraries.  I would recommend using pip. 
+### Libraries: numpy, pandas, scipy
+### You may also want to install tensorflow or pytorch to try some ML methods.
